cef048c35819f79bb3f37e2ed41942ff36866a2f
dhananjay <dhananjaydeswal11@gmail.com>
2020-03-28 05:06:13 +0530

    new file added

---
+++ testing.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class App extends React.Component{
+  render(){
+    return(
+        <View>
+        	<Text>oh yeah</Text>
+        	<Text>oh yeah</Text>
+        	<Text>oh yeah</Text>
+        	<Text>oh yeah</Text>
+        	<Text>oh yeah</Text>
+        	<Text>oh yeah</Text>
+        </View>
+      )
+  }
+}
+
+
+
+export default App;
