539d4150b5482d49691a62c8483d0b473f8c1008
dhananjay <dhananjaydeswal11@gmail.com>
2020-03-28 17:39:12 +0530

    testing2 added

---
+++ testing2.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class App extends React.Component{
+  render(){
+    return(
+        <View>
+        	<Text>oh yeah 2</Text>
+        	<Text>oh yeah 2</Text>
+        </View>
+      )
+  }
+}
+
+
+
+export default App;
